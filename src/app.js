267240a4048742b37fa9a9d29819269092e816f3
Mateo <mateogarcia1660@gmail.com>
2024-04-23 22:21:17 -0300

    0.05

--- src/app.js
+++ src/app.js
@@ -1,21 +1,76 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+import { engine } from "express-handlebars"
 
-const productRouter=require("./routes/productRouter")
-const cartRouter=require("./routes/cartRouter")
+const productRouter = require("./routes/productRouter")
+const cartRouter = require("./routes/cartRouter")
+const realTimeProductsRouter = require('./routes/realTimeProductsRouter');
 
 const PORT = 3000;
+
 const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static("./public"))
+
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+app.set("views", "./views")
 
 app.use("/api/products", productRouter)
 app.use("/api/carts", cartRouter)
+app.use("/api/realTimeProducts", realTimeProductsRouter)
 
 app.get('/',(req,res)=>{
     res.setHeader('Content-Type','text/plain');
     res.status(200).send('OK');
 })
 
+io.on("connection", (socket) => {
+    console.log("Nuevo cliente conectado");
+    
+    
+    socket.on("addProduct", async (newProduct) => {
+      try {
+        
+  
+        
+        io.emit("productAdded", newProduct);
+      } catch (error) {
+        console.error("Error al agregar el producto:", error);
+      }
+    });
+  
+   
+    socket.on("updateProduct", async (updatedProduct) => {
+      try {
+        
+  
+        
+        io.emit("productUpdated", updatedProduct);
+      } catch (error) {
+        console.error("Error al actualizar el producto:", error);
+      }
+    });
+  
+    
+    socket.on("deleteProduct", async (deletedProduct) => {
+      try {
+        
+  
+        
+        io.emit("productDeleted", deletedProduct);
+      } catch (error) {
+        console.error("Error al eliminar el producto:", error);
+      }
+    });
+  });
 
 app.listen(PORT, () => console.log(`Servidor en línea en el puerto ${PORT}`));
+
+module.exports = { io };
--- src/routes/realTimeProductsRouter.js
+++ src/routes/realTimeProductsRouter.js
@@ -1,42 +1,18 @@
-import { Router } from 'express';
-export const router=Router()
-
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const { io } = require('../app');
 const ProductManager = require("../classes/ProductManager");
 
 const productFilePath = path.join(__dirname, '..', 'data', 'products.json');
 const productManagerInstance = new ProductManager(productFilePath);
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        let limit = req.query.limit; 
-        let productos = await productManagerInstance.getProducts();
-
-        // Verificar si se proporcionó un límite y si es válido
-        if (limit && !isNaN(limit) && parseInt(limit) > 0) {
-            productos = productos.slice(0, parseInt(limit));
-        }
-
-        // res.json(productos);
-        res.render("pages", { body: "pages", products: productos });
+        const products = await productManagerInstance.getProducts();
+        res.render('realTimeProducts', { products });
     } catch (error) {
         res.status(500).json({ error: error.message });
-    }
-});
-
-router.get("/:pid", async (req, res) => {
-    try {
-        // Verificar si se proporcionó el parámetro de la ruta ":id"
-        if (!req.params.id || isNaN(req.params.pid)) {
-            throw new Error("ID de producto inválido");
-        }
-
-        const productId = parseInt(req.params.id); 
-
-        const product = await productManagerInstance.getProductById(productId);
-        
-        res.json(product);
-    } catch (error) {
-        res.status(404).json({ error: error.message });
     }
 });
 
@@ -44,7 +20,6 @@ router.post("/", async (req, res) => {
     try {
         const { title, description, price, code, stock, category, thumbnails } = req.body;
 
-        // Asegurar que se proporcionen todos los campos requeridos
         if (!title || !description || !price || !code || !stock || !category) {
             throw new Error('Todos los campos son requeridos, excepto "thumbnails"');
         }
@@ -56,13 +31,16 @@ router.post("/", async (req, res) => {
             code,
             stock,
             category,
-            thumbnails: thumbnails || [], // Si no se proporciona, se establece como un array vacío
-            status: true // Establecer el estado como true por defecto
+            thumbnails: thumbnails || [],
+            status: true
         });
 
         console.log(`Producto creado exitosamente: ${newProduct}`);
 
-        res.status(201).json(newProduct); // Devolver el nuevo producto creado
+        // Emitir evento de WebSocket con los productos actualizados
+        io.emit('productAdded', newProduct);
+
+        res.status(201).json(newProduct);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
@@ -70,23 +48,24 @@ router.post("/", async (req, res) => {
 
 router.put("/:pid", async (req, res) => {
     try {
-        const productId = parseInt(req.params.pid); 
+        const productId = parseInt(req.params.pid);
+        const updatedFields = req.body;
 
-        const updatedFields = req.body; // Obtener los campos actualizados del cuerpo de la solicitud
-
-        // Verificar si se proporcionaron campos actualizados
         if (!Object.keys(updatedFields).length) {
             throw new Error("No se proporcionaron campos para actualizar");
         }
 
-        const product = await productManagerInstance.getProductById(productId); // Verificar si el producto existe
+        const product = await productManagerInstance.getProductById(productId);
         if (!product) {
             throw new Error("Producto no encontrado");
         }
 
         const updatedProduct = await productManagerInstance.updateProduct(productId, updatedFields);
-        
+
         console.log(`Producto actualizado correctamente: ${updatedProduct}`);
+
+        // Emitir evento de WebSocket con el producto actualizado
+        io.emit('productUpdated', updatedProduct);
 
         res.json(updatedProduct);
     } catch (error) {
@@ -94,12 +73,11 @@ router.put("/:pid", async (req, res) => {
     }
 });
 
-
 router.delete("/:pid", async (req, res) => {
     try {
-        const productId = parseInt(req.params.pid); 
+        const productId = parseInt(req.params.pid);
 
-        const product = await productManagerInstance.getProductById(productId); // Verificar si el producto existe
+        const product = await productManagerInstance.getProductById(productId);
         if (!product) {
             throw new Error("Producto no encontrado");
         }
@@ -107,9 +85,14 @@ router.delete("/:pid", async (req, res) => {
         const deletedProduct = await productManagerInstance.deleteProduct(productId);
 
         console.log(`Producto eliminado correctamente: ${deletedProduct}`);
-        
+
+        // Emitir evento de WebSocket con el producto eliminado
+        io.emit('productDeleted', deletedProduct);
+
         res.json(deletedProduct);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 });
+
+module.exports = router;
